5b0ccdba91f3d1c2578b90b808abaa6496cd6fba
lucime470 <759812597@qq.com>
2025-02-09 00:56:35 +0800

    Site updated: 2025-02-09 00:56:35

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1739033363192";const preCache = ["/images/taichi.png","/images/banner1.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1739033786118";const preCache = ["/images/taichi.png","/images/banner1.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
